9c2b635119c6f5eaf3318b2d3eb133d100c7e4da
DKeddyK <d.keddyk.k@googlemail.com>
2021-08-16 13:26:37 +0200

    re-enabled service workers

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,6 +11,7 @@ module.exports = {
       // proxy URLs to backend development server
       '/api': 'http://localhost:9000',
     },
+    port: '9000',
     contentBase: path.join(__dirname, 'dist'), // boolean | string | array, static file location
     compress: true, // enable gzip compression
     historyApiFallback: true, // true for index.html upon 404, object for multiple paths
